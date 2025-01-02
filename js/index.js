1d1fb2544e7ff26fa1a53328a2dc0240a222139b
dale murphy <smcpsycho51@gmail.com>
2025-01-02 10:18:33 -0500

    updated readme and attached answeres.txt

--- js/index.js
+++ js/index.js
@@ -2,12 +2,38 @@
 // that are not declared
 "use strict"
 
-let x = 10;
+//let x = 10;
 
-let a = 5;
-let b = 2;
-let c = a + b;
+//let a = 5;
+//let b = 2;
+//let c = a + b;
 
-let a = 5;
-let b = 2;
-let c = a * b;
+//let a = 5;
+//let b = 2;
+//let c = a * b;
+
+//let x = 10;
+//x += 5;
+
+//let letter1 = "A";
+//let letter2 = "B";
+//let result = letter1 < letter2;
+
+//let firstName = "John";
+//let lastName = "Deere";
+//let fullName = firstName + " " + lastName;
+
+//let first = 5 + 5;
+//let second = "5" + 5;
+//let third = "Dale" + 51;
+
+//x = 6 y = 3
+//(x < 10 && y > 1) 
+
+//(x == 5 || y == 5)
+//(x == 6 || y == 0)
+//(x == 0 || y == 3)
+//(x == 6 || y == 3)
+
+//!(x === y) 
+//!(x > y)
